f0dce3eb4ebbff889e83e39f32f65c30574e4fe1
Max Plomer <maxplomer@gmail.com>
2016-08-04 20:35:04 -0400

    make sample API call

--- app.jsx
+++ app.jsx
@@ -6,13 +6,21 @@ var Battleship = React.createClass({
     return myState;
   },
 
+  componentDidMount: function() {
+    this.serverRequest = $.get('http://localhost:3001/ping', function (result) {
+      this.setState({
+        message: result
+      });
+    }.bind(this));
+  },
+
   componentDidUpdate: function() {
   },
 
   render: function() {
     return (
       <div>
-        Helloworld
+        Helloworld { this.state.message }
       </div>
     );
   }
