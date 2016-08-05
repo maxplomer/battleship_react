602ffd3d266cee14334fe4d7881e4c8a6bcafc1c
Max Plomer <maxplomer@gmail.com>
2016-08-04 20:22:50 -0400

    initial commit

---
+++ app.jsx
@@ -0,0 +1,21 @@
+var Battleship = React.createClass({
+  getInitialState: function() {
+    var myState = {
+    };
+
+    return myState;
+  },
+
+  componentDidUpdate: function() {
+  },
+
+  render: function() {
+    return (
+      <div>
+        Helloworld
+      </div>
+    );
+  }
+});
+
+React.render(<Battleship/>, document.body);
